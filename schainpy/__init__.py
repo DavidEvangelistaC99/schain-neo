ef10f4ea4261fb8372d56b503202d3b3d5789288
DavidEvangelistaC99 <david.evangelista.c@gmail.com>
2026-03-16 12:13:11 -0500

    .py

---
+++ schainpy/__init__.py
@@ -0,0 +1,8 @@
+"""Signal chain python package"""
+
+try:
+    from schainpy.controller import Project
+except:
+    pass
+
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import numpy
 
 ext_modules = [
     Extension(
-        "schainpy.model._noise",
+        "schainpy.model.data._noise",
         sources=["schainc/_noise.c"],
         include_dirs=[numpy.get_include()],
     ),
     Extension(
-        "schainpy.model._HS_algorithm",
+        "schainpy.model.data._HS_algorithm",
         sources=["schainc/_HS_algorithm.c"],
         include_dirs=[numpy.get_include()],
     ),
